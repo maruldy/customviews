518a4559ba2ad2a0a49d969436c3e23fbc29182c
DaeYoung Lee <maruldy@gmail.com>
2017-04-22 13:31:20 +0900

    Initial commit

---
+++ app/src/main/java/com/maruldy/cvs/izimageview/IZImageView.java
@@ -0,0 +1,25 @@
+package com.maruldy.cvs.izimageview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by maruldy on 2017-04-22.
+ */
+
+public class IZImageView extends android.support.v7.widget.AppCompatImageView {
+    public IZImageView(Context context) {
+        super(context);
+    }
+
+    public IZImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IZImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
